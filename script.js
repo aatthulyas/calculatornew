4b6f4d2cf5efda6a580b1db01fc46822ab56c6a6
Athulya <athulya703@gmail.com>
2021-11-25 21:15:07 +0530

    initial

---
+++ script.js
@@ -0,0 +1,24 @@
+function displayValue(num)
+{
+    let res=document.querySelector("#result")
+    res.value+=num
+    // only id not class****    var displayValue=(num)=>(result.value+=num)
+}
+var clearBox=()=>result.value=""
+// function clearBox(){
+//     document.querySelector("#result").value=" "
+    
+// }
+//eval expression for evaluation
+
+var evaluateExpression=()=>result.value=eval(result.value)
+
+
+
+// function evaluateExpression()
+// {
+//     let res=document.querySelector("#result")
+//     let cur_val=res.value
+//     let result=eval(cur_val)
+//     res.value=result}
+
